d12facf82648cb5c7adb3141af5fd8565641cc64
Riccardo Mija <riccardomp@gmail.com>
2021-04-09 15:24:16 -0500

    Clase 4 - Update

--- app/src/main/java/pe/com/bootcamp/retrofitmvvm/viewmodel/BCPViewModel.kt
+++ app/src/main/java/pe/com/bootcamp/retrofitmvvm/viewmodel/BCPViewModel.kt
@@ -5,6 +5,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import pe.com.bootcamp.retrofitmvvm.data.remote.Result
 import kotlinx.coroutines.launch
@@ -13,9 +14,10 @@ import pe.com.bootcamp.retrofitmvvm.data.entities.dashboard.DashboardResponse
 import pe.com.bootcamp.retrofitmvvm.data.entities.discount.Discount
 import pe.com.bootcamp.retrofitmvvm.data.entities.discount.DiscountResponse
 import pe.com.bootcamp.retrofitmvvm.data.repository.BCPRepository
+import javax.inject.Inject
 
-
-class BCPViewModel @ViewModelInject constructor(private val repository: BCPRepository) :
+@HiltViewModel
+class BCPViewModel @Inject constructor(private val repository: BCPRepository) :
     BaseViewModel() {
 
     private val _dashboard = MutableLiveData<DashboardResponse>()
